eafd2eff55e576bae7a942dfb9f0ea91040f8135
charmaine wang <charmainewang424@hotmail.com>
2019-04-17 11:40:02 +0200

    first commit

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Message;
+
+class MessagesController extends Controller
+{
+
+
+     public function index()
+    {
+        $user = User::find(1);
+
+        return $user;
+        $messages = Message::all()->where('sender_id', $user->id);
+
+        return view('index')->with(['name' => $user->name, 'messages' => $messages]);
+    }
+
+
+
+}
